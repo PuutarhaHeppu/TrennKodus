dd26d2d7082600ae05227cfd10411d1d67b3cd16
Opilane TTHK <opilane@tthk.ee>
2024-10-10 12:40:16 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,20 @@ app.get("/harjutused", (req, res) => {
     res.send(["rinnalt surumine", "kükid"])
 })
 
+app.post("/harjutused", (req, res) => {
+    if (!req.body.name || req.body.name.trim().length === 0){
+        return res.status(400).send({error: "Missing required field 'name'"})
+    }
+    const newKordused = parseFloat(req.body.korudsed);
+    const newHarjutus = {
+        id: createId(),
+        name: req.body.name,
+        kordused: isNaN(newKordused) ? null : newKordused
+        }
+    harjutused.push(newHarjutus)
+    res.send(harjutused)
+})
+ 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })

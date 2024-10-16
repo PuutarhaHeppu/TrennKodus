593c805b13e961a731720e029cd03e9ddb6b5378
Mona Lisa <Mona.Lisa@gmail.com>
2024-10-16 21:14:34 +0300

    peatykk 26

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,79 @@
+const express = require('express')
+const app = express()
 const port = 8080
-const app = require("express")()
 const swaggerUi = require("swagger-ui-express")
-const swaggerDoc = require("./docs/swagger.json")
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
-const harjutused = [
-    {id: 1, name: "push-up", repetition: 10},
-    {id: 2, name: "Diamond push-up", repetition: 10},
-    {id: 3, name: "squat", kordused: 10},
-    {id: 4, name: "pistol squat", kordused: 10},
-    {id: 5, name: "lat pull down", kordused: 10}
+app.use(express.json())
+
+const exercises = [
+    {id: 1, name: "push-up", repetition: 10, set: 3},
+    {id: 2, name: "Diamond push-up", repetition: 10, set: 3},
+    {id: 3, name: "squat", repetition: 10, set: 3},
+    {id: 4, name: "pistol squat", repetition: 10, set: 3},
+    {id: 5, name: "lat pull down", repetition: 10, set: 3}
 ]
 
-app.get("/harjutused", (req, res) => {
-    if (typeof harjutused[req.params.id - 1] === 'undefined'){
+app.get("/exercises/:id", (req, res) => {
+    if (typeof exercises[req.params.id - 1] === 'undefined'){
         return res.status(404).send({error: "Exercise not found"})
     }
 })
 
-app.get('/harjutused/:id', (req, res) => {
-    res.send(harjutused[req.params.id - 1])
+app.get('/exercises/:id', (req, res) => {
+    res.send(exercises[req.params.id - 1])
 })
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
-
-
-app.post("/harjutused", (req, res) => {
-    if (!req.body.name || req.body.name.trim().length === 0){
+app.post('/exercises', (req, res) => {
+    if (!req.body.name || !req.body.set || !req.body.repesition){
         return res.status(400).send({error: "Missing required field 'name'"})
     }
-    const newKordused = parseFloat(req.body.korudsed);
-    const newHarjutus = {
-        id: createId(),
+    let exercise = {
+        id: exercises.length + 1,
         name: req.body.name,
-        set: isNaN(newSet) ? null : newSet,
-        repetition: isNaN(newRepetition) ? null : newRepetition
-        }
-    harjutused.push(newHarjutus)
-    res.send(harjutused)
+        set: req.body.set,
+        repetition: req.body.repetition,
+        // set: isNaN(newSet) ? null : newSet,
+        // repetition: isNaN(newRepetition) ? null : newRepetition
+    }
+
+    exercises.push(exercise)
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/exercises/${exercises.length}`)
+        .send(exercise)
 })
+
+app.delete('/exercises/id:', (req, res) => {
+    if (typeof exercises[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Exercise not found"})
+    }
+
+    exercises.splice(req.params.id -1, 1)
+
+    res.status(204).send({error: "No content"})
+})
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + `://${req.headers.host}`
+}
+
+//  app.post("/exercises", (req, res) => {
+//      if (!req.body.name || req.body.name.trim().length === 0){
+//          return res.status(400).send({error: "Missing required field 'name'"})
+//      }
+//      const newKordused = parseFloat(req.body.korudsed);
+//      const newExercise = {
+//          id: createId(),
+//          name: req.body.name,
+//          set: isNaN(newSet) ? null : newSet,
+//          repetition: isNaN(newRepetition) ? null : newRepetition
+//          }
+//      exercises.push(newExercise)
+//      res.send(exercises)
+//  })
  
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)

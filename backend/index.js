b9b29593618c714d838cbec1003276d3d5d5d5ac
Mona Lisa <Mona.Lisa@gmail.com>
2024-10-17 11:39:33 +0300

    add post exercises endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,78 +2,58 @@ const express = require('express')
 const app = express()
 const port = 8080
 const swaggerUi = require("swagger-ui-express")
-const yamljs = require('yamljs')
-const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json())
 
 const exercises = [
-    {id: 1, name: "push-up", repetition: 10, set: 3},
-    {id: 2, name: "Diamond push-up", repetition: 10, set: 3},
-    {id: 3, name: "squat", repetition: 10, set: 3},
-    {id: 4, name: "pistol squat", repetition: 10, set: 3},
-    {id: 5, name: "lat pull down", repetition: 10, set: 3}
+    {id: 1, name: "push-up", repetitions: 10, sets: 3},
+    {id: 2, name: "Diamond push-up", repetitions: 10, sets: 3},
+    {id: 3, name: "squat", repetitions: 10, sets: 3},
+    {id: 4, name: "pistol squat", repetitions: 10, sets: 3},
+    {id: 5, name: "lat pull down", repetitions: 10, sets: 3}
 ]
 
-app.get("/exercises/:id", (req, res) => {
-    if (typeof exercises[req.params.id - 1] === 'undefined'){
-        return res.status(404).send({error: "Exercise not found"})
+app.get("/exercises", (req, res) => {
+    res.send(exercises.map(({id,name}) => {
+        return {id, name}
+    }))
+})
+
+function createId() {
+    const maxIdExercise = exercises.reduce((prev, current) => (prev.id > current.id) ? prev : current, 1)
+    return maxIdExercise.id + 1;
+}
+
+app.post('/exercises', (req, res) => {
+    if (!req.body.name || req.body.name.trim().length === 0){
+        return res.status(400).send({error: "Missing required field 'name'"})
     }
+    const newExercise = {
+        id: createId(),
+        name: req.body.name,
+        sets: req.body.sets,
+        repetitions: req.body.repetitions
+    }
+    exercises.push(newExercise)
+    res.status(201)
+        .location(`http://$localhost:${port}/exercises/${newExercise.id}`)
+        .send(newExercise)
 })
 
 app.get('/exercises/:id', (req, res) => {
-    res.send(exercises[req.params.id - 1])
-})
-
-app.post('/exercises', (req, res) => {
-    if (!req.body.name || !req.body.set || !req.body.repesition){
-        return res.status(400).send({error: "Missing required field 'name'"})
+    const idNumber = parseInt(req.params.id)
+    if (isNaN(idNumber)) {
+        return res.status(400).send({error: `ID must be a whole number: ${req.params.id}`})
     }
-    let exercise = {
-        id: exercises.length + 1,
-        name: req.body.name,
-        set: req.body.set,
-        repetition: req.body.repetition,
-        // set: isNaN(newSet) ? null : newSet,
-        // repetition: isNaN(newRepetition) ? null : newRepetition
+    const exercise = exercises.find(e => e.id === idNumber)
+    if (!exercise) {
+        return res.status(404).send({error: `Exercise not found`})
     }
-
-    exercises.push(exercise)
-
-    res.status(201)
-        .location(`${getBaseUrl(req)}/exercises/${exercises.length}`)
-        .send(exercise)
+    res.send(exercise)
 })
-
-app.delete('/exercises/id:', (req, res) => {
-    if (typeof exercises[req.params.id - 1] === 'undefined') {
-        return res.status(404).send({error: "Exercise not found"})
-    }
-
-    exercises.splice(req.params.id -1, 1)
-
-    res.status(204).send({error: "No content"})
-})
-
-function getBaseUrl(req) {
-    return req.connection && req.connection.encrypted
-        ? 'https' : 'http' + `://${req.headers.host}`
-}
-
-//  app.post("/exercises", (req, res) => {
-//      if (!req.body.name || req.body.name.trim().length === 0){
-//          return res.status(400).send({error: "Missing required field 'name'"})
-//      }
-//      const newKordused = parseFloat(req.body.korudsed);
-//      const newExercise = {
-//          id: createId(),
-//          name: req.body.name,
-//          set: isNaN(newSet) ? null : newSet,
-//          repetition: isNaN(newRepetition) ? null : newRepetition
-//          }
-//      exercises.push(newExercise)
-//      res.send(exercises)
-//  })
  
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)

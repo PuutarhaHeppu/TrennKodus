ea24fb39cf3b0992dde49ec9d21cb1439607d4dd
Marken Lemming <Marken.lemming@gmail.com>
2024-11-27 10:25:08 +0200

    commit et muudatused alles jätta

---
+++ frontend/src/components/ExerciseTable.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "ExercisesTable",
+    props:{
+        items: Array
+    },
+}
+</script>
+
+<template>
+<table class="table table-striped">
+    <thead class="table-dark">
+    <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>MuscleGroup</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in items">
+        <td>{{ item.name}}</td>
+        <td>{{ item.description}}</td>
+        <td>{{ item.muscleGroup}}</td>
+    </tr>
+    <tr>
+        <td>Push-up</td>
+        <td>An exercise in wich a person keeping a prone position with the hands palms down under the shoulders the balls of the feet on the ground, and the back straight pushes the body up and lets it down by an alternate straightening and bending of the arms.</td>
+        <td>Chest</td>
+    </tr>
+    </tbody>
+
+</table>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import ExercisesTable from '../components/ExercisesTable.vue';
+export default {
+  components: {
+    ExercisesTable,
+  },
+  data() {
+    return {
+      allExercises: []
+    }
+  },
+  async created() {
+    this.allExercises = await (await fetch('http://localhost:5173/exercises')).json()
+  }
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ExercisesTable :items=allExercises/>
   </main>
 </template>
